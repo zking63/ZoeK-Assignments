452c449cddd55205a22dbf7564674cff529f0e2f
zking63 <zoemking63@gmail.com>
2021-02-11 10:11:42 -0500

    2.11.21.2

---
+++ 01-java-fundamentals/07-map-of-the-hashmatique/Hashmatique.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Set;
+public class Hashmatique{
+    public void Hashmatique(){
+        HashMap<String, String> trackList = new HashMap<String, String>();
+        trackList.put("Dear Mama", "When I was young, me and my mama had beef \n  Seventeen years old, kicked out on the streets");
+        trackList.put("California Love", "California knows how to party \n  In the city of L.A \n  In the city of good ol' Watts");
+        trackList.put("Keep Ya Head Up", "Some say the blacker the berry, the sweeter the juice");
+        trackList.put("All Bout U", "Ahh yeah \n  Yeauhh! \n  It's all about you, one time! \n  I'ma say it's all about you baby, yeah");
+        for(String key : trackList.keySet()) {
+            System.out.println((String.format("Track: %s: Lyrics: %s", key, trackList.get(key))));
+        }
+        System.out.println("Some of the lyrics to Dear Mama: "+trackList.get("Dear Mama"));
+    }
+}
---
+++ 01-java-fundamentals/07-map-of-the-hashmatique/HashmatiqueTest.java
@@ -0,0 +1,7 @@
+public class HashmatiqueTest{
+    public static void main(String[] args){
+        Hashmatique output = new Hashmatique();
+
+        output.Hashmatique();
+    }
+}
