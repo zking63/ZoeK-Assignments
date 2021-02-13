7b179b7cc902c32b2ad890798bba4410d9a34f86
zking63 <zoemking63@gmail.com>
2021-02-13 11:22:25 -0500

    2.13.21.2

--- 02-java-oop/01-project-class/Project.java
+++ 02-java-oop/01-project-class/Project.java
@@ -1,18 +1,20 @@
 public class Project {
     private String name;
     private String description;
+    private double initialCost = 0;
     private static String defaultName = "Default Name";
     private static String defaultD = "Default Description";
 
     public String getName() { return name; }
     public String getDescription() { return description; }
+    public Double getInitialCost() { return initialCost; }
 
     public void setName(String name) { this.name = name; }
     public void setDescription(String description) {this.description = description; }
+    public void setInitialCost() { this.initialCost = initialCost; }
 
     public String elevatorPitch(){
-        System.out.println(name + ": " + description);
-        return String.format("%s: %s", name, description);
+        return String.format("%s:(%f) %s", name, initialCost, description);
     }
     public Project() {
         this.name = defaultName;
@@ -29,6 +31,6 @@ public class Project {
     public Project(String name, String description) {
         this.name = name;
         this.description = description;
-        System.out.println(name + ": " + description);
+        System.out.println(name + ": " + initialCost + ": " + description);
     }
 }
--- 02-java-oop/01-project-class/ProjectTest.java
+++ 02-java-oop/01-project-class/ProjectTest.java
@@ -1,13 +1,7 @@
 public class ProjectTest {
     public static void main(String[] args){
-        Project result = new Project();
+        Project result = new Project("Ed", "Zoe");
 
-        result.elevatorPitch();
-
-        /*result.Project1();
-
-        result.Project1("hello");
-
-        result.Project1("Hello", "World");*/
+        System.out.println(result.elevatorPitch());
     }
 }
