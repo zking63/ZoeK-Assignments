465ea6690847e1a69e6b08513964899ba2f47d89
zking63 <zoemking63@gmail.com>
2021-02-17 08:16:49 -0500

    2.17.21.2

--- 02-java-oop/07-bank-account/BankAccount.java
+++ 02-java-oop/07-bank-account/BankAccount.java
@@ -1,16 +1,32 @@
+import java.util.Random;
 public class BankAccount {
     private String accountNumber;
     private double checkingBalance; 
     private double savingsBalance;
     private static int numberofAccounts = 0;
-    public BankAccount(String accountNumber, double checkingBalance, double savingsBalance){
-        this.accountNumber = accountNumber;
+    private static int totalMoney = 0;
+
+    private static String getAcctNumber(){
+        String acct = "";
+        Random x = new Random();
+        for (int i = 0; i < 1; i++){
+            acct += x.nextInt(4);
+        }
+        return acct;
+    }
+
+    public BankAccount(double checkingBalance, double savingsBalance){
+        this.accountNumber = getAcctNumber();
         this.checkingBalance = checkingBalance;
         this.savingsBalance = savingsBalance;
+        totalMoney += this.checkingBalance + this.savingsBalance;
         System.out.println("Account number: " + accountNumber + "\nChecking Balance: " + checkingBalance + "\nSavings Balance: " + savingsBalance);
         numberofAccounts++;
     }
     public static int getnumberofAccounts(){
         return numberofAccounts;
+    }
+    public static int getTotalMoney(){
+        return totalMoney;
     }
 }
--- 02-java-oop/07-bank-account/BankAccountTest.java
+++ 02-java-oop/07-bank-account/BankAccountTest.java
@@ -1,10 +1,15 @@
 public class BankAccountTest {
     public static void main(String[] args){
-        BankAccount account1 = new BankAccount("1111", 20.20, 21.21);
+        BankAccount account1 = new BankAccount(20, 10);
 
-        BankAccount account2 = new BankAccount("2222", 30.20, 41.21);
+        BankAccount account2 = new BankAccount(10, 20);
+
+        BankAccount account3 = new BankAccount(10, 20);
+
+        BankAccount account4 = new BankAccount(10, 20);
 
         //System.out.println(account1);
         System.out.println(BankAccount.getnumberofAccounts());
+        System.out.println(BankAccount.getTotalMoney());
     }
 }
