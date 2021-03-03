1098939f99ea43924392cd216b7b032fdc3916b5
zking63 <zoemking63@gmail.com>
2021-03-03 15:06:05 -0500

    Show2

--- 04-java-spring/02-spring-data-i/02show/src/main/java/com/codingdojo/show/service/BookService.java
+++ 04-java-spring/02-spring-data-i/02show/src/main/java/com/codingdojo/show/service/BookService.java
@@ -19,14 +19,6 @@ private final BookRepository bookRepository;
  public BookService(BookRepository bookRepository) {
      this.bookRepository = bookRepository;
  }
- // initialize the books variable with values
- private List<Book> books = new ArrayList(Arrays.asList(
-         new Book("Harry Potter and the Sorcerer's Stone", "A boy wizard saving the world", "english", 309),
-         new Book("The Great Gatsby", "The story primarily concerns the young and mysterious millionaire Jay Gatsby", "english", 180),
-         new Book("Moby Dick", "The saga of Captain Ahab", "english", 544),
-         new Book("Don Quixote", "Life of a retired country gentleman", "english", 150),
-         new Book("The Odyssey", "Ancient Greek epic poem", "english", 475)
-         ));
  // returns all the books
  public List<Book> allBooks() {
      return bookRepository.findAll();
