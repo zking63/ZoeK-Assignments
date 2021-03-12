b32e7b183150ba3a9a53442695791515b94afc5e
zking63 <zoemking63@gmail.com>
2021-03-12 14:38:08 -0500

    PC3

--- 04-java-spring/03-spring-data-ii/05productscategories/src/main/java/com/codingdojo/productscategories/controllers/PCController.java
+++ 04-java-spring/03-spring-data-ii/05productscategories/src/main/java/com/codingdojo/productscategories/controllers/PCController.java
@@ -1,7 +1,76 @@
 package com.codingdojo.productscategories.controllers;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.codingdojo.productscategories.models.Category;
+import com.codingdojo.productscategories.models.Product;
+import com.codingdojo.productscategories.services.PCService;
+
 public class PCController {
+	private final PCService pcservice;
+	public PCController (PCService pcservice) {
+		this.pcservice = pcservice;
+	}
 	public String index() {
 		return "Hello world";
+	}
+	@RequestMapping("/product/new")
+	public String newProduct(@ModelAttribute("product")Product product) {
+		return "/product/newproduct.jsp";
+	}
+	@RequestMapping(value="/product", method=RequestMethod.POST)
+	public String createProduct(@Valid @ModelAttribute("product") Product product, BindingResult result) {
+		if(result.hasErrors()) {
+			return "product/newproduct.jsp";
+		}
+		else {
+			pcservice.createProduct(product);
+			return "redirect:/";
+		}
+	}
+	@RequestMapping("/category/new")
+	public String newCategory(@ModelAttribute("category")Category category) {
+		return "/product/newcategory.jsp";
+	}
+	@RequestMapping(value="/category", method=RequestMethod.POST)
+	public String createCategory(@Valid @ModelAttribute("category") Category category, BindingResult result) {
+		if(result.hasErrors()) {
+			return "category/newcategory.jsp";
+		}
+		else {
+			pcservice.createCategory(category);
+			return "redirect:/";
+		}
+	}
+	@RequestMapping("/product/{id}")
+	public String showProduct(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("product", pcservice.findProduct(id));
+		return "/product/showproduct.jsp";
+	}
+	@RequestMapping("/product/{id}")
+	public String addCategory(@ModelAttribute("product")Product product, Model model) {
+		List<Category> categories = pcservice.getCategories();
+		model.addAttribute("categories", categories);
+		return "/product/showproduct.jsp";
+	}
+	@RequestMapping("/category/{id}")
+	public String showCategory(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("category", pcservice.findCategory(id));
+		return "/product/showproduct.jsp";
+	}
+	@RequestMapping("/category/{id}")
+	public String addProduct(@ModelAttribute("category")Category category, Model model) {
+		List<Product> products = pcservice.getProducts();
+		model.addAttribute("products", products);
+		return "/category/showcategory.jsp";
 	}
 }
--- 04-java-spring/03-spring-data-ii/05productscategories/src/main/java/com/codingdojo/productscategories/services/PCService.java
+++ 04-java-spring/03-spring-data-ii/05productscategories/src/main/java/com/codingdojo/productscategories/services/PCService.java
@@ -1,7 +1,11 @@
 package com.codingdojo.productscategories.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.codingdojo.productscategories.models.Category;
+import com.codingdojo.productscategories.models.Product;
 import com.codingdojo.productscategories.repositories.CategoryRepo;
 import com.codingdojo.productscategories.repositories.ProductRepo;
 
@@ -12,5 +16,23 @@ public class PCService {
 	public PCService(ProductRepo prepo, CategoryRepo crepo) {
 		this.crepo =crepo;
 		this.prepo = prepo;
+	}
+	public List<Product> getProducts() {
+		return prepo.findAll();
+	}
+	public List<Category> getCategories() {
+		return crepo.findAll();
+	}
+	public Product createProduct(Product p) {
+		return prepo.save(p);
+	}
+	public Category createCategory(Category c) {
+		return crepo.save(c);
+	}
+	public Product findProduct(Long id) {
+		return prepo.findById(id).orElse(null);
+	}
+	public Category findCategory(Long id) {
+		return crepo.findById(id).orElse(null);
 	}
 }
