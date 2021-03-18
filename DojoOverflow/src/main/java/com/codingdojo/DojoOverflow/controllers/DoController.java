8d99736392a6d7684f7bdd3abcf5b6efc011028d
zking63 <zoemking63@gmail.com>
2021-03-17 20:46:16 -0400

    DojoOverflow initial commit

---
+++ DojoOverflow/src/main/java/com/codingdojo/DojoOverflow/controllers/DoController.java
@@ -0,0 +1,10 @@
+package com.codingdojo.DojoOverflow.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DoController {
+	public String index() {
+		return "hello world";
+	}
+}
