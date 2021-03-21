ee96674d98e3a50edc89086d9abf69f8091ba30e
zking63 <zoemking63@gmail.com>
2021-03-21 10:26:12 -0400

    user service created

---
+++ 04-java-spring/05-login-and-registration/authentication/src/main/java/com/codingdojo/authentication/services/UserService.java
@@ -0,0 +1,56 @@
+package com.codingdojo.authentication.services;
+
+import java.util.Optional;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+import com.codingdojo.authentication.models.User;
+import com.codingdojo.authentication.repositories.UserRepo;
+@Service
+public class UserService {
+    private final UserRepo urepo;
+    
+    public UserService(UserRepo urepo) {
+        this.urepo = urepo;
+    }
+    
+// register user and hash their password
+    public User registerUser(User user) {
+        String hashed = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+        user.setPassword(hashed);
+        return urepo.save(user);
+    }
+    
+// find user by email
+    public User findByEmail(String email) {
+        return urepo.findByEmail(email);
+    }
+    
+    // find user by id
+    public User findUserById(Long id) {
+    	Optional<User> u = urepo.findById(id);
+    	
+    	if(u.isPresent()) {
+            return u.get();
+    	} else {
+    	    return null;
+    	}
+    }
+    
+// authenticate user
+    public boolean authenticateUser(String email, String password) {
+        // first find the user by email
+        User user = urepo.findByEmail(email);
+        // if we can't find it by email, return false
+        if(user == null) {
+            return false;
+        } else {
+// if the passwords match, return true, else, return false
+            if(BCrypt.checkpw(password, user.getPassword())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
+
