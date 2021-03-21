64f4f18d84d0220201f7169fbdc691a61001057e
zking63 <zoemking63@gmail.com>
2021-03-21 10:15:36 -0400

    controller added

---
+++ 04-java-spring/05-login-and-registration/authentication/src/main/java/com/codingdojo/authentication/controllers/AController.java
@@ -0,0 +1,10 @@
+package com.codingdojo.authentication.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AController {
+	public String index() {
+		return "hello world";
+	}
+}
