74d7d3f6ae20f1f541f4de27f32a2413e0f370ec
zking63 <zoemking63@gmail.com>
2021-03-22 07:39:06 -0400

    controller class created

---
+++ 04-java-spring/05-login-and-registration/events/src/main/java/com/codingdojo/events/controller/EventsController.java
@@ -0,0 +1,10 @@
+package com.codingdojo.events.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EventsController {
+	public String index() {
+		return "hello world";
+	}
+}
